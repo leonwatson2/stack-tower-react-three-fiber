1e32467ed3d4a74f045fdd87fa37361d6d56a49b
leonwatson2 <leon@vlw2.com>
2023-04-21 15:02:06 -0500

    whitespace

--- src/index.tsx
+++ src/index.tsx
@@ -12,25 +12,25 @@ import { KeyboardControls, KeyboardControlsEntry } from '@react-three/drei';
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 const map: KeyboardControlsEntry<Controls>[] = [
-  { name: Controls.hit, keys: ['Space'] },
-  { name: Controls.reset, keys: ['r'] },
+    { name: Controls.hit, keys: ['Space'] },
+    { name: Controls.reset, keys: ['r'] },
 ];
 
 root.render(
-  <KeyboardControls map={map}>
-    <Leva />
-    <Canvas
-      id={'game-canvas'}
-      camera={{
-        zoom: 150,
-        near: -50,
-        rotation: [0, 0, 0],
-        position: [1, 1, 1],
-      }}
-      orthographic
-    >
-      <Experience />
-    </Canvas>
+    <KeyboardControls map={map}>
+        <Leva />
+        <Canvas
+            id={'game-canvas'}
+            camera={{
+                zoom: 150,
+                near: -50,
+                rotation: [0, 0, 0],
+                position: [1, 1, 1],
+            }}
+            orthographic
+        >
+            <Experience />
+        </Canvas>
     ,
-  </KeyboardControls>,
+    </KeyboardControls>,
 );
